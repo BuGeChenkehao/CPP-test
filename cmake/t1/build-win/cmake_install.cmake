83ade2860ccea0558738f48b75f67583f9acbbfe
StephenNG59 <15920902266@139.com>
2019-07-07 11:39:04 +0800

    .

--- cmake/t1/build-win/cmake_install.cmake
+++ cmake/t1/build-win/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/ZJU/ZJU-/20190705短学期C++项目管理及工程实践/CPP-test/cmake/t1
+# Install script for directory: E:/dev/Projects/CPP-test/cmake/t1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/ZJU/ZJU-/20190705短学期C++项目管理及工程实践/CPP-test/cmake/t1/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/dev/Projects/CPP-test/cmake/t1/build-win/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
